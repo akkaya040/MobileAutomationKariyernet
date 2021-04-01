43e40358918fac5b46af6b9ec7a8ef6ec5079dbd
Kurtulus <akkaya040@gmail.com>
2021-04-01 19:19:49 +0300

    Initial commit

---
+++ src/test/java/ObjectsKariyernet.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+
+public class ObjectsKariyernet {
+
+    public static final By txtUsername = By.id("com.kariyer.androidproject:id/edt_email");
+    public static final By txtPassword = By.id("com.kariyer.androidproject:id/edt_password");
+    public static final By btnLogin = By.id("com.kariyer.androidproject:id/fl_login");
+    public static final By btnSignup = By.id("com.kariyer.androidproject:id/txt_register");
+
+    public static final By txtName = By.id("com.kariyer.androidproject:id/name");
+    public static final By txtSurname = By.id("com.kariyer.androidproject:id/surname");
+    public static final By txtEmail = By.id("com.kariyer.androidproject:id/email");
+    public static final By txtPass = By.id("com.kariyer.androidproject:id/password");
+
+    public static final By cbxServicesAgreement = By.id("com.kariyer.androidproject:id/serviceAgreementChoice");
+    public static final By btnConfirm = By.id("com.kariyer.androidproject:id/button_confirm");
+    public static final By btnReject = By.id("com.kariyer.androidproject:id/button_reject");
+
+    public static final By cbxContractPermission = By.id("com.kariyer.androidproject:id/contact_permission");
+    public static final By cbxKvkk = By.id("com.kariyer.androidproject:id/kvkkChoice");
+
+    public static final By btnCreateAccount = By.id("com.kariyer.androidproject:id/txt_register");
+    public static final By btnLetsStart = By.id("com.kariyer.androidproject:id/start");
+    public static final By btnNo = By.id("com.kariyer.androidproject:id/btnNo");
+    public static final By btnYes = By.id("com.kariyer.androidproject:id/btnYes");
+    public static final By btnSkip = By.id("com.kariyer.androidproject:id/skip");
+
+
+}
